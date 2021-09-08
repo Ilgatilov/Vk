a665f26ff81579a5e497984cf6fd1af98399a920
Ilya Gatilov <depoldominic@gmail.com>
2021-09-08 15:43:03 +0300

    initial commit

---
+++ src/main/java/ru/netology/domain/GeolocationInfo.java
@@ -0,0 +1,7 @@
+package ru.netology.domain;
+
+public class GeolocationInfo {
+    private String type;
+    private String coordinates;
+    private String place;
+}
---
+++ src/main/java/ru/netology/domain/Post.java
@@ -0,0 +1,22 @@
+package ru.netology.domain;
+
+public class Post {
+    private int recordId;
+    private int ownerId;
+    private CommentsInfo commentsInfo;
+    private LikesInfo likesInfo;
+    private ViewsInfo viewsInfo;
+    private RepostInfo repostInfo;
+    private GeolocationInfo geolocationInfo;
+    private int date;
+    private int communityId;
+    private int time;
+    private int PhotoCommunityId;
+    private boolean InBookmark;
+    private int haveTimer;
+    private int pinPost;
+    private int deletePost;
+    private int editPost;
+    private String postText;
+    private int advertisingInfo;
+}
