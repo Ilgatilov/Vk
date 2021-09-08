af0601bc12e4a093c66bd4a4fa08250e9b8f6c16
Ilya Gatilov <depoldominic@gmail.com>
2021-09-08 16:20:06 +0300

    added new classes

--- src/main/java/ru/netology/domain/Post.java
+++ src/main/java/ru/netology/domain/Post.java
@@ -19,4 +19,8 @@ public class Post {
     private int editPost;
     private String postText;
     private int advertisingInfo;
+    // версия 2.0
+    private CopyrightInfo copyrightInfo;
+    private PostSource postSource;
+    private DonutInfo donutInfo;
 }
